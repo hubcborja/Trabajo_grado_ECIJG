d065f5974ee3cd559a7c14baf9de3fa59f6dbecc
Huber Camilo Borja <huber.c@hotmail.com>
2026-06-06 22:26:18 -0500

    change in R script, bibliography and folder
    Change in R script of ERGm application, bibliography folder update with new references and creating new folder for python colab scripts

--- aplicacionERGM.R
+++ aplicacionERGM.R
@@ -1,3 +1,4 @@
+# Liberias
 #install.packages("tidyverse")
 #install.packages("igraph")
 #install.packages("ergm.count")
@@ -14,89 +15,62 @@ library(ergm.count)
 library(ergm.rank)
 library(latentnet)
 
+# Importación de la base
 baseERGM <- read.csv("bases_datos/base_comercio_ERGM.csv")
 
-
+# Datos para 2014 y dejar las primeras columnas según los códigos ISO de primeras
 baseERGM24 <- baseERGM[baseERGM$year==2024, ]
 baseERGM24 <- baseERGM24[, -c(1,4,5,7,8)]
-#com_network <- baseERGM24[, c("iso3_o", "iso3_d", "peso_comercio_total")]
-#com_network <- com_network[com_network$iso3_o!=com_network$iso3_d,]
 
-network_comtrade <- as.network(baseERGM24[, 1:3], directed=TRUE, matrix.type = "edgelist", ignore.eval = FALSE,
-                               loops=FALSE)
+baseERGM24 <- baseERGM24 %>%
+  mutate(across(where(is.numeric), ~ifelse(is.na(.), median(., na.rm = TRUE), .)))
+
+# Crear la red (Con dirección, sin self-loops, matriz tipo desde lista de aristas)
+network_comtrade <- as.network(baseERGM24[, c(1,2,3)], directed=TRUE, matrix.type = "adjacency", 
+                               ignore.eval = FALSE, loops=FALSE)
+# Estadística de la red
 summary(network_comtrade)
 network.vertex.names(network_comtrade)
 
 colnames(baseERGM24)
 
-A <- igraph::as_adjacency_matrix(graph = network_comtrade)
-isSymmetric(as.matrix(A))
+#A <- igraph::as_adjacency_matrix(graph = network_comtrade)
+#isSymmetric(as.matrix(A))
 
-network::set.vertex.attribute(x = network_comtrade, attrname = "CDT_vigente",    value = baseERGM24$CDT_vigente)
-network::set.vertex.attribute(x = network_comtrade, attrname = "norm_ln_dist",  value = baseERGM24$norm_ln_dist)
-network::set.vertex.attribute(x = network_comtrade, attrname = "contig",    value = baseERGM24$contig)
-network::set.vertex.attribute(x = network_comtrade, attrname = "comlang_off", value = baseERGM24$comlang_off)
-network::set.vertex.attribute(x = network_comtrade, attrname = "comrelig",    value = baseERGM24$comrelig)
-network::set.vertex.attribute(x = network_comtrade, attrname = "fta_wto",  value = baseERGM24$fta_wto)
+#Atributos de las aristas
+network::set.edge.attribute(x = network_comtrade, attrname = "CDT_vigente",    value = baseERGM24$CDT_vigente)
+network::set.edge.attribute(x = network_comtrade, attrname = "norm_ln_dist",  value = baseERGM24$norm_ln_dist)
+network::set.edge.attribute(x = network_comtrade, attrname = "contig",    value = baseERGM24$contig)
+network::set.edge.attribute(x = network_comtrade, attrname = "comlang_off", value = baseERGM24$comlang_off)
+network::set.edge.attribute(x = network_comtrade, attrname = "comrelig",    value = baseERGM24$comrelig)
+network::set.edge.attribute(x = network_comtrade, attrname = "fta_wto",  value = baseERGM24$fta_wto)
 network::set.vertex.attribute(x = network_comtrade, attrname = "norm_ln_pib_per_capita_dolares_constantes_o",    value = baseERGM24$norm_ln_pib_per_capita_dolares_constantes_o)
 network::set.vertex.attribute(x = network_comtrade, attrname = "norm_ln_pib_per_capita_dolares_constantes_d", value = baseERGM24$norm_ln_pib_per_capita_dolares_constantes_d)
 network::set.vertex.attribute(x = network_comtrade, attrname = "norm_entry_cost_o",    value = baseERGM24$norm_entry_cost_o)
 network::set.vertex.attribute(x = network_comtrade, attrname = "norm_entry_cost_d",  value = baseERGM24$norm_entry_cost_d)
 
-peso_comercio <- baseERGM24 %v% 'peso_comercio_total'
+peso_comercio <- baseERGM24[, c(3)]
 peso_comercio
 
+#graficación de los nodos
 plot(network_comtrade, main = "peso_comercio_total", label = network.vertex.names(network_comtrade))
-plot(network_comtrade, vertex.cex = peso_comercio, main = "peso_comercio_total")
+plot(network_comtrade, edge.cex = peso_comercio, main = "peso_comercio_total")
 
-attr_comercio <- network_comtrade %v% network_comtrade$peso_comercio_total
-
-
-ergm_aristas_triangulos <- network_comtrade~edges+ triangles
-summary(ergm_aristas_triangulos)
-ergm.fit.01 <- ergm(formula = ergm_aristas_triangulos)
-summary(ergm.fit.01)
-
-
-
-ergm_aristas_triangulos <- network_comtrade~edges+ match("CDT_vigente")+ 
-  match("contig")+
-  nodemain("norm_ln_dist")+
-  nodemain("comlang_off")+
-  nodemain("comrelig")+
-  match("fta_wto")+
+#Establecimiento del ERGM (endogenidad y valores exógenos)
+ergm_aristas_triangulos <- network_comtrade ~ sum + mutual(form="min") +
+  edgecov(network_comtrade, "CDT_vigente")+ 
+  edgecov(network_comtrade, "contig")+
+  edgecov(network_comtrade, "norm_ln_dist")+
+  edgecov(network_comtrade, "comlang_off")+
+  edgecov(network_comtrade, "comrelig")+
+  edgecov(network_comtrade, "fta_wto")+
   nodemain("norm_ln_pib_per_capita_dolares_constantes_o")+
   nodemain("norm_ln_pib_per_capita_dolares_constantes_d")+
   nodemain("norm_entry_cost_o")+
   nodemain("norm_entry_cost_d")
 
-summary(ergm_aristas_triangulos)
-ergm.fit.01 <- ergm(formula = ergm_aristas_triangulos)
+summary(ergm_aristas_triangulos, response = "peso_comercio_total")
+ergm.fit.01 <- ergm(formula = ergm_aristas_triangulos, response='peso_comercio_total', reference = ~Poisson)
 summary(ergm.fit.01)
 
-network_comtrade <- as.network(baseERGM24[, 1:2], 
-                               directed = TRUE, 
-                               matrix.type = "edgelist")
 
-
-set.edge.value(network_comtrade, "comercio_valor", baseERGM24[, 3])
-
-
-summary(network_comtrade, verbose = FALSE)
-nombres_nodos <- network.vertex.names(network_comtrade)
-nombres_nodos
-
-atributos_nodos <- baseERGM24 %>%
-  select(iso3_o, norm_ln_pib_per_capita_dolares_constantes_o, CDT_vigente) %>%
-  distinct(iso3_o, .keep_all = TRUE)
-atributos_nodos
-# Mapeamos los atributos a la red[cite: 1, 2]
-set.vertex.attribute(network_comtrade, "pib_capita", atributos_nodos$norm_ln_pib_per_capita_dolares_constantes_o)
-set.vertex.attribute(network_comtrade, "CDT", atributos_nodos$CDT_vigente)
-
-
-modelo_test <- ergm(network_comtrade ~ sum + mutual, 
-                    response = "comercio_valor", 
-                    reference = ~Poisson)
-
-summary(modelo_test)
